6bb73136760ade9269760cd6468021cc61b1f712
Pedrosavioo <pedrosaviolima618@gmail.com>
2023-10-12 12:33:53 -0300

    alinhando conteúdo no centro

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import icon from './images/icon-star.svg';
 
 export default function App() {
   return (
-    <div className="container">
-      <div className='icon'>
-        <img src={icon}/>
+    <div className='center'>
+      <div className="container">
+        <div className='icon'>
+          <img src={icon}/>
+        </div>
+        <h1>How did we do?</h1>
+        <p>Please let us know how we did with your support request. All feedback is appreciated 
+    to help us improve our offering!</p>
+        <div className='feedback'>
+          <div className='nota'>1</div>
+          <div className='nota'>2</div>
+          <div className='nota'>3</div>
+          <div className='nota'>4</div>
+          <div className='nota'>5</div>
+        </div>
+        <div className='submit'>Submit</div>
       </div>
-      <h1>Como Fizemos?</h1>
-      <p>Informe-nos como nos saímos com sua solicitação de suporte. Todos os comentários são apreciados
-      para nos ajudar a melhorar nossa oferta!</p>
-      <div className='feedback'>
-        <div className='nota'>1</div>
-        <div className='nota'>2</div>
-        <div className='nota'>3</div>
-        <div className='nota'>4</div>
-        <div className='nota'>5</div>
-      </div>
-      <div className='submit'>Submit</div>
     </div>
   );
 }
