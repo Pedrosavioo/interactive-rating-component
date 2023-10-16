64363b244a37a60ed077e6bf010c2c9aef389d92
Pedrosavioo <pedrosaviolima618@gmail.com>
2023-10-16 13:38:56 -0300

    component-feedback

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import './App.css';
-import icon from './images/icon-star.svg';
-
+import Feedback from './componentes/feedback'
 export default function App() {
+  // transformar div container em componente
   return (
     <div className='center'>
-      <div className="container">
-        <div className='icon'>
-          <img src={icon}/>
-        </div>
-        <h1 className='mt-2'>How did we do?</h1>
-        <p className='mt-1'>Please let us know how we did with your support request. All feedback is appreciated 
-    to help us improve our offering!</p>
-        <div className='feedback mt-2'>
-          <div className='nota'>1</div>
-          <div className='nota'>2</div>
-          <div className='nota'>3</div>
-          <div className='nota'>4</div>
-          <div className='nota'>5</div>
-        </div>
-        <div className='submit mt-2'>Submit</div>
-      </div>
+      <Feedback />
     </div>
-  );
+  )
 }
